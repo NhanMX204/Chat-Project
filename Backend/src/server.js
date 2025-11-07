f02b91e055cd8d25ebe6410aa20202eb4d6ccbab
Mai Nhan <nhan54v@gmail.com>
2025-11-07 12:25:38 +0700

    fe

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import { protectRoute } from "./middlewares/authMiddleware.js";
+import cors from "cors";
 
 
 dotenv.config();
@@ -15,6 +16,10 @@ const PORT = process.env.PORT || 5001;
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}))
 
 //public route
 app.use("/api/auth", authRoute);
